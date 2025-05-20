5191325d1660f67314323a38435901208a28b2b4
Saeed <Saeedmh8908@gmail.com>
2025-05-20 18:03:35 +0330

    add expense dto and expense entity

---
+++ src/main/java/com/mh/expensemanager/entity/Expense.java
@@ -0,0 +1,34 @@
+package com.mh.expensemanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "tbl_expenses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String expenseId;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal amount;
+
+    private Date date;
+
+    private String dateString;
+
+}
