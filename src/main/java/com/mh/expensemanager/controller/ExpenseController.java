a11b31507f9ad9ec7dd4efb20697e9d61a55a722
Saeed <Saeedmh8908@gmail.com>
2025-05-26 20:48:47 +0330

    show expenses in thymelead from database

--- src/main/java/com/mh/expensemanager/controller/ExpenseController.java
+++ src/main/java/com/mh/expensemanager/controller/ExpenseController.java
@@ -1,13 +1,20 @@
 package com.mh.expensemanager.controller;
 
+import com.mh.expensemanager.service.ExpenseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ExpenseController {
 
+    private final ExpenseService expenseService;
+
     @GetMapping("/expenses")
-    public String showExpenseList() {
+    public String showExpenseList(Model model) {
+        model.addAttribute("expenses", expenseService.getAllExpenses());
         return "expenses-list";
     }
 }
